9ab0572f899e717c39695213297bf3710de43186
dineshg0pal <dineshpost0@example.com>
2026-02-24 01:27:18 -0500

    updated the input to support caps lock, shift, ctrl

--- keyboard.c
+++ keyboard.c
@@ -1,7 +1,6 @@
-// keyboard.c
-
 #include <stdint.h>
 #include "vga.h"
+#include "keyboard.h"
 
 #define PIC1_COMMAND 0x20
 #define PIC_EOI      0x20
@@ -10,7 +9,6 @@
 #define INPUT_BUFFER_SIZE 256
 
 /* ---------------- I/O ---------------- */
-
 static inline uint8_t inb(uint16_t port)
 {
     uint8_t ret;
@@ -23,25 +21,29 @@ static inline void outb(uint16_t port, uint8_t val)
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
-/* ---------------- SCANCODE TABLE ---------------- */
+/* ---------------- MODIFIERS ---------------- */
+static uint8_t shift = 0;
+static uint8_t caps  = 0;
+static uint8_t ctrl  = 0;
 
+/* ---------------- SCANCODE TABLES ---------------- */
 static const char scancode_table[128] = {
     0,  27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b',
-    '\t',
-    'q','w','e','r','t','y','u','i','o','p','[',']','\n',
-    0,
-    'a','s','d','f','g','h','j','k','l',';','\'','`',
-    0,
-    '\\',
-    'z','x','c','v','b','n','m',',','.','/',
-    0,
-    '*',
-    0,
-    ' ',
+    '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',
+    0,'a','s','d','f','g','h','j','k','l',';','\'','`',
+    0,'\\','z','x','c','v','b','n','m',',','.','/',
+    0,'*',0,' '
+};
+
+static const char scancode_shifted[128] = {
+    0, 27, '!','@','#','$','%','^','&','*','(',')','_','+','\b',
+    '\t','Q','W','E','R','T','Y','U','I','O','P','{','}','\n',
+    0,'A','S','D','F','G','H','J','K','L',':','"','~',
+    0,'|','Z','X','C','V','B','N','M','<','>','?',
+    0,'*',0,' '
 };
 
 /* ---------------- RING BUFFER ---------------- */
-
 static char input_buffer[INPUT_BUFFER_SIZE];
 static uint32_t head = 0;
 static uint32_t tail = 0;
@@ -66,20 +68,59 @@ char keyboard_getchar()
 }
 
 /* ---------------- IRQ HANDLER ---------------- */
-
 void irq1_handler()
 {
     uint8_t scancode = inb(KEYBOARD_DATA_PORT);
 
-    if (scancode & 0x80) {
+    /* Handle key release */
+    uint8_t released = scancode & 0x80;
+    uint8_t code = scancode & 0x7F;
+
+    /* Modifier keys */
+    switch(code) {
+        case 0x2A: // Left Shift
+        case 0x36: // Right Shift
+            shift = !released;
+            break;
+        case 0x3A: // Caps Lock
+            if(!released) caps = !caps;
+            break;
+        case 0x1D: // Ctrl
+            ctrl = !released;
+            break;
+    }
+
+    if(released) {
         outb(PIC1_COMMAND, PIC_EOI);
         return;
     }
 
-    char c = scancode_table[scancode];
+    /* Ignore unknown scancodes */
+    if(code >= 128) {
+        outb(PIC1_COMMAND, PIC_EOI);
+        return;
+    }
 
-    if (c)
+    /* Translate to character */
+    char c;
+    if(shift)
+        c = scancode_shifted[code];
+    else
+        c = scancode_table[code];
+
+    /* Apply Caps Lock for letters */
+    if(c >= 'a' && c <= 'z') {
+        if(shift ^ caps) c -= 32; // make uppercase
+    }
+
+    /* Ctrl combinations (example Ctrl+C prints ^C) */
+    if(ctrl) {
+        if(c >= 'a' && c <= 'z') c -= 96; // ASCII control code
+    }
+
+    if(c)
         buffer_push(c);
 
+    /* Send EOI */
     outb(PIC1_COMMAND, PIC_EOI);
 }
---
+++ keyboard.h
@@ -0,0 +1,12 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <stdint.h>
+
+/* Returns 0 if no key pressed, otherwise ASCII character */
+char keyboard_getchar();
+
+/* IRQ handler called from IDT */
+void irq1_handler();
+
+#endif
