d8e2194cc2961aea39654d4821a9efc448a9787d
dineshg0pal <dineshpost0@gmail.com>
2026-03-06 08:38:48 -0500

    feat: added a time-stamp log in the shell cmd - dmsg

--- klog.c
+++ klog.c
@@ -1,5 +1,6 @@
 #include "klog.h"
 #include "vga.h"
+#include "pit.h"
 
 /* ------------------------------------------------ */
 /* Simple Bee Kernel Logger                         */
@@ -10,9 +11,41 @@
 static char klog_buffer[KLOG_BUFFER_SIZE];
 static int klog_index = 0;
 
-/* very small string copy */
-static void klog_store(const char* msg)
+/* small helper: print hex ticks */
+static void klog_print_ticks(uint32_t ticks)
 {
+    char hex[11] = "0x00000000";
+    const char* digits = "0123456789ABCDEF";
+
+    for (int i = 0; i < 8; i++)
+    {
+        hex[9 - i] = digits[(ticks >> (i * 4)) & 0xF];
+    }
+
+    print(hex);
+}
+
+/* store log inside buffer */
+static void klog_store(uint32_t ticks, const char* msg)
+{
+    /* store timestamp */
+
+    if (klog_index < KLOG_BUFFER_SIZE - 12)
+    {
+        klog_buffer[klog_index++] = '[';
+
+        for (int i = 7; i >= 0; i--)
+        {
+            char digit = "0123456789ABCDEF"[(ticks >> (i * 4)) & 0xF];
+            klog_buffer[klog_index++] = digit;
+        }
+
+        klog_buffer[klog_index++] = ']';
+        klog_buffer[klog_index++] = ' ';
+    }
+
+    /* store message */
+
     while (*msg && klog_index < KLOG_BUFFER_SIZE - 1)
     {
         klog_buffer[klog_index++] = *msg++;
@@ -26,11 +59,16 @@ static void klog_store(const char* msg)
 
 void klog(const char* msg)
 {
-    print("[BEE] ");
+    uint32_t ticks = pit_get_ticks();
+
+    print("[");
+    klog_print_ticks(ticks);
+    print("] ");
+
     print(msg);
     print("\n");
 
-    klog_store(msg);
+    klog_store(ticks, msg);
 }
 
 /* dump entire log buffer */
